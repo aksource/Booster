2450c18ba7bddf61d25248b6447351710c1f7749
aksource <akira.mew@gmail.com>
2014-03-06 22:32:58 +0900

    Add LifeStream Fluid

--- java/Booster/ItemBooster.java
+++ java/Booster/ItemBooster.java
@@ -5,7 +5,7 @@ import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 
 public class ItemBooster extends ItemArmor {
-	private String boostername;
+	public String boostername;
 	public ItemBooster(ItemArmor.ArmorMaterial par2EnumArmorMaterial, int par3, int par4, String name) {
 		super(par2EnumArmorMaterial, par3, par4);
 		this.setTextureName(Booster.TextureDomain + name);
@@ -16,10 +16,13 @@ public class ItemBooster extends ItemArmor {
     public String getArmorTexture(ItemStack stack, Entity entity, int slot, String layer)
 	{
 		String st = "";
-		if(boostername.equals("Booster08") /*stack.getItem() == Booster.Booster08*/)
-			st = (layer.equals(""))? Booster.TextureDomain + Booster.Armor08_1:Booster.TextureDomain + Booster.Armor08_2;
-		else if(boostername.equals("Booster20")/*stack.getItem() == Booster.Booster20*/)
-			st = (layer.equals(""))? Booster.TextureDomain + Booster.Armor20_1:Booster.TextureDomain + Booster.Armor20_2;
+        if (stack != null || stack.getItem() instanceof ItemBooster) {
+            ItemBooster booster = (ItemBooster)stack.getItem();
+            if(booster.boostername.equals("Booster08") /*stack.getItem() == Booster.Booster08*/)
+                st = (!layer.equals(""))? Booster.TextureDomain + Booster.Armor08_1:Booster.TextureDomain + Booster.Armor08_2;
+            else if(booster.boostername.equals("Booster20")/*stack.getItem() == Booster.Booster20*/)
+                st = (!layer.equals(""))? Booster.TextureDomain + Booster.Armor20_1:Booster.TextureDomain + Booster.Armor20_2;
+        }
     	return st;
 	}
 }
